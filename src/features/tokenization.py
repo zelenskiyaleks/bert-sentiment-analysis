8ffa8106e5940f2aa04e33c5426d87533eaebef4
Aleksandr Zelenskiy <zelenskiyaleks@gmail.com>
2026-05-17 14:49:23 +0100

    Add tokenization pipeline

---
+++ src/features/tokenization.py
@@ -0,0 +1,21 @@
+from transformers import AutoTokenizer
+
+def load_tokenizer(model_name):
+    """
+    Load tokenizer for a transformer model.
+    """
+    return AutoTokenizer.from_pretrained(model_name)
+
+def tokenize_batch(
+    examples,
+    tokenizer,
+    max_length = 512
+    ):
+    """
+    Tokenize batch of texts.
+    """
+    return tokenizer(
+        examples["text"],
+        truncation=True,
+        max_length=max_length
+    )
